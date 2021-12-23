fe15e89ffece3e51a97e4ea0e2a6428d5734ac45
prathjan <prathjan@cisco.com>
2021-12-23 01:31:59 -0800

    moved back to 8

--- scripts/appd.sh
+++ scripts/appd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "cisco ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/cisco
 apt update
-apt install openjdk-11-jdk -y
+apt install openjdk-8-jdk -y
 apt install curl -y
 apt install jq -y
 
